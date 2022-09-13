a872d240769c0424feed705110a0c947bc5ad3ce
stellaubara <stella.ubara@yahoo.com>
2022-09-13 15:41:16 -0700

    a program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,15 @@
 #include "main.h"
+
 /**
  * main - Entry point
  *
  * *Return: 0 nothing, return void
- */
+*/
+
 int main(void)
+
 {
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -16,5 +20,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
